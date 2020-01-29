9f7e55f5926c2c95263b0bea5040d3c0e44639f8
cch57 <christopher.hazen@duke.edu>
2020-01-28 22:58:50 -0500

    updated view contructor

--- src/cellsociety/View.java
+++ src/cellsociety/View.java
@@ -3,7 +3,7 @@ package cellsociety;
 import javafx.scene.Group;
 
 public class View {
-  public View(Group viewGroup, int widthCells, int heightCells){
+  public View(Group viewGroup, int widthCells, int heightCells, Model currentModel){
 
   }
 
