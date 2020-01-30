fc9e32bf2fb824afa750922a2de3e2f0c2c86453
lubenjamin <lubenjamin@gmail.com>
2020-01-30 01:59:01 -0500

    swapped grid from arraylist to array

--- src/cellsociety/Model.java
+++ src/cellsociety/Model.java
@@ -1,29 +1,25 @@
 package cellsociety;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Model {
 
-  private ArrayList<List<Cell>> grid = new ArrayList<List<Cell>>();
+  private Cell[][] grid;
   private ArrayList<Cell> neighborhood = new ArrayList<Cell>();
-  private int rows;
-  private int columns;
 
   public Model(int widthCells, int heightCells){
-    rows = heightCells;
-    columns = widthCells;
+    grid = new Cell[heightCells][widthCells];
   }
 
   public Cell getCell(int x, int y){
-    return grid.get(x).get(y);
+    return grid[x][y];
   }
 
   public ArrayList<Cell> getNeighborhood(int x, int y){
-    neighborhood.add(grid.get(x).get(y + 1));
-    neighborhood.add(grid.get(x).get(y - 1));
-    neighborhood.add(grid.get(x + 1).get(y));
-    neighborhood.add(grid.get(x - 1).get(y));
+    neighborhood.add(grid[x][y + 1]);
+    neighborhood.add(grid[x][y - 1]);
+    neighborhood.add(grid[x + 1][y]);
+    neighborhood.add(grid[x - 1][y]);
     return neighborhood;
   }
 }
