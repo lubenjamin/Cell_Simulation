2bd20801b81ac8fc396d4b0f626b226d87e0d181
cch57 <christopher.hazen@duke.edu>
2020-01-30 15:41:18 -0500

    added method signatures for controller

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -6,6 +6,7 @@ public class Cell {
   private String currentState;
   private String nextState;
   private Color displayColor;
+
   public Cell(String initialState, Color initialColor){
     currentState = initialState;
     displayColor = initialColor;
--- src/cellsociety/Controller.java
+++ src/cellsociety/Controller.java
@@ -3,8 +3,27 @@ package cellsociety;
 import javafx.scene.Group;
 
 public class Controller {
+  private Model currentModel;
+  private View currentView;
   public Controller (Group simGroup){
+    currentModel = new Model(100,100);
+    currentView = new View(simGroup, 100, 100, currentModel);
+  }
+
+  public void updateSim(){
+    updateGrid();
+    swtichGridStates();
+    updateView();
+  }
+  private void updateGrid(){
 
   }
+  private void swtichGridStates(){
+
+  }
+  private void updateView(){
+    
+  }
+
 
 }
