d5f17e1c309f6f637b9884a4959ad5168fffe007
lubenjamin <lubenjamin@gmail.com>
2020-01-30 01:22:57 -0500

    added basic model methods

--- src/cellsociety/Model.java
+++ src/cellsociety/Model.java
@@ -1,24 +1,25 @@
 package cellsociety;
 
 import java.util.ArrayList;
-
-/*
--Make grid
--Get cells
--getNeighborhood
- */
+import java.util.List;
 
 public class Model {
-  
-  public Model(int widthCells, int heightCells){
 
+  private ArrayList<List<Cell>> grid = new ArrayList<List<Cell>>();
+  private ArrayList<Cell> neighborhood = new ArrayList<Cell>();
+
+  public Model(int widthCells, int heightCells){
   }
 
-  public Cell getCell(double x, double y){
-
+  public Cell getCell(int x, int y){
+    return grid.get(x).get(y);
   }
 
   public ArrayList<Cell> getNeighborhood(int x, int y){
-
+    neighborhood.add(grid.get(x).get(y + 1));
+    neighborhood.add(grid.get(x).get(y - 1));
+    neighborhood.add(grid.get(x + 1).get(y));
+    neighborhood.add(grid.get(x - 1).get(y));
+    return neighborhood;
   }
 }
