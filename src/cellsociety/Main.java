4ed13dfcff100a49f35d28be1910c1f782611d27
cch57 <christopher.hazen@duke.edu>
2020-01-30 21:57:49 -0500

    stashing for merge

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -18,7 +18,7 @@ import javafx.util.Duration;
  */
 public class Main extends Application {
 
-  private static final int FRAMES_PER_SECOND = 24t;
+  private static final int FRAMES_PER_SECOND = 24;
   private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
   private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
   private Controller currentController;
