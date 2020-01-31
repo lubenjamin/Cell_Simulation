69b096a8f76928ac99eee820755ab7cedffd41be
lubenjamin <lubenjamin@gmail.com>
2020-01-30 21:59:05 -0500

    file broken

--- src/cellsociety/FileReader.java
+++ src/cellsociety/FileReader.java
@@ -1,43 +1,43 @@
-package cellsociety;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-/**
- * simulation size
- * probabilities
- * five titles
- * initial states (segregation)
- *
- */
-
-public class FileReader {
-    public static final String ERROR_MESSAGE = "XML file does not represent %s";
-    private final String TYPE_ATTRIBUTE;
-    private final DocumentBuilder DOCUMENT_BUILDER;
-
-    public XMLParser (String type) {
-        DOCUMENT_BUILDER = getDocumentBuilder();
-        TYPE_ATTRIBUTE = type;
-    }
-
-
-    private DocumentBuilder getDocumentBuilder () {
-        try {
-            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        }
-        catch (ParserConfigurationException e) {
-            throw new XMLException(e);
-        }
-    }
-
-}
+//package cellsociety;
+//
+//import javax.xml.parsers.DocumentBuilder;
+//import javax.xml.parsers.DocumentBuilderFactory;
+//import javax.xml.parsers.ParserConfigurationException;
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+//import org.w3c.dom.NodeList;
+//import org.xml.sax.SAXException;
+//
+///**
+// * simulation size
+// * probabilities
+// * five titles
+// * initial states (segregation)
+// *
+// */
+//
+//public class FileReader {
+//    public static final String ERROR_MESSAGE = "XML file does not represent %s";
+//    private final String TYPE_ATTRIBUTE;
+//    private final DocumentBuilder DOCUMENT_BUILDER;
+//
+//    public XMLParser (String type) {
+//        DOCUMENT_BUILDER = getDocumentBuilder();
+//        TYPE_ATTRIBUTE = type;
+//    }
+//
+//
+//    private DocumentBuilder getDocumentBuilder () {
+//        try {
+//            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+//        }
+//        catch (ParserConfigurationException e) {
+//            throw new XMLException(e);
+//        }
+//    }
+//
+//}
