2cde5a62e14ad8d258aa121c1d3673b82cf4735e
Abhijay Suhag <as866@duke.edu>
2020-02-01 21:54:42 -0500

    view is flexible with xml

--- src/View/View.java
+++ src/View/View.java
@@ -8,15 +8,16 @@ import javafx.scene.paint.Paint;
 import java.util.ArrayList;
 
 public class View {
-  private final int SPACING = 6;
-  private final int VISUAL_WIDTH = 5;
-  private final int VISUAL_HEIGHT = 5;
+  private final int SPACING = 1;
+  private final int VIEW_BOUND = 400;
+  private double myVisualWidth;
+  private double myVisualHeight;
 
   private Model myModel;
   private Group myViewGroup;
   private ArrayList<CellVisual> myVisuals = new ArrayList<>();
 
-  public View(Group viewGroup, double widthCells, double heightCells, Model currentModel) {
+  public View(Group viewGroup, int widthCells, int heightCells, Model currentModel) {
     this.myModel = currentModel;
     this.myViewGroup = viewGroup;
     initCellView(currentModel, widthCells, heightCells);
@@ -31,12 +32,14 @@ public class View {
       cv.setFill(Color.WHITE);
     }
   }
-  private void initCellView(Model grid, double width, double height) {
+  private void initCellView(Model grid, int width, int height) {
+    myVisualWidth = VIEW_BOUND/width;
+    myVisualHeight = VIEW_BOUND/height;
     for (int i = 0; i < width; i++) {
       for (int j = 0; j < height; j++) {
-        CellVisual cv = new CellVisual(VISUAL_WIDTH, VISUAL_HEIGHT, displayState(grid.getCell(i, j)), i, j);
-        cv.setX(i * SPACING);
-        cv.setY(j * SPACING);
+        CellVisual cv = new CellVisual(myVisualWidth, myVisualHeight, displayState(grid.getCell(i, j)), i, j);
+        cv.setX(i*(myVisualWidth+SPACING));
+        cv.setY(j*(myVisualHeight+SPACING));
         myVisuals.add(cv);
         myViewGroup.getChildren().add(cv);
       }
