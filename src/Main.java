4938868663260742f3012c6aa97c362063e6f96f
cch57 <christopher.hazen@duke.edu>
2020-02-06 16:05:59 -0500

    pushing for merge

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main  extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage){
     Simulator sim = new Simulator();
     sim.initialize(primaryStage);
   }
