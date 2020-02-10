80c717a32cb34dbf00d1a8f2d1b5adc3413f727b
lubenjamin <lubenjamin@gmail.com>
2020-02-09 23:18:01 -0500

    abstracted square and hex model

--- src/utils/Model.java
+++ src/utils/Model.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Model {
+public abstract class Model {
 
   private final Cell[][] grid;
   public boolean manualEntry;
@@ -30,56 +30,6 @@ public class Model {
 
   public List<Cell> getSimpleNeighborhood(int x, int y) {
     ArrayList<Cell> ret = new ArrayList<>();
-    addIfExists(ret, x, y + 1);
-    addIfExists(ret, x - 1, y);
-    addIfExists(ret, x + 1, y);
-    addIfExists(ret, x, y - 1);
-    return ret;
-  }
-
-  public List<Cell> getTorusNeighborhood(int x, int y) {
-    ArrayList<Cell> ret = new ArrayList<>();
-    addTorus(ret, x, y + 1);
-    addTorus(ret, x - 1, y);
-    addTorus(ret, x + 1, y);
-    addTorus(ret, x, y - 1);
-    return ret;
-  }
-
-  private void addTorus(ArrayList<Cell> ret, int x, int y) {
-    int newX = x;
-    int newY = y;
-    if (x >= grid[0].length) {
-      newX = 0;
-    }
-    if (x < 0) {
-      newX = grid[0].length - 1;
-    }
-    if (y >= grid.length) {
-      newY = 0;
-    }
-    if (y < 0) {
-      newY = grid.length - 1;
-    }
-
-    ret.add(grid[newY][newX]);
-  }
-
-
-  public List<Cell> getMooreNeighborhood(int x, int y) {
-    ArrayList<Cell> ret = new ArrayList<>();
-
-    addIfExists(ret, x - 1, y + 1);
-    addIfExists(ret, x, y + 1);
-    addIfExists(ret, x + 1, y + 1);
-
-    addIfExists(ret, x - 1, y);
-    addIfExists(ret, x + 1, y);
-
-    addIfExists(ret, x - 1, y - 1);
-    addIfExists(ret, x, y - 1);
-    addIfExists(ret, x + 1, y - 1);
-
     return ret;
   }
 
