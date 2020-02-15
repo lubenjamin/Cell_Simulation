4421c430a2d9f1450b0a71b28c2d894d4f3fe50b
Abhijay Suhag <abhijay.suhag@duke.edu>
2020-02-15 02:43:04 -0500

    comment CellVisual.java

--- src/View/CellVisual.java
+++ src/View/CellVisual.java
@@ -9,6 +9,14 @@ public class CellVisual extends Rectangle {
   final int myYPos;
   final Paint myColor;
 
+  /** create a visualization of a cell object held in model that corresponds to cell state
+   *
+   * @param width
+   * @param height
+   * @param color
+   * @param x - i index of model
+   * @param y - j index of model
+   */
   public CellVisual(double width, double height, Paint color, int x, int y) {
     this.myXPos = x;
     this.myYPos = y;
@@ -18,11 +26,18 @@ public class CellVisual extends Rectangle {
     this.setFill(color);
   }
 
+  /**
+   * @return i index of this visual corresponding to model
+   */
   public int getXPos() {
     return myXPos;
   }
 
+  /**
+   * @return j index of this visual corresponding to model
+   */
   public int getYPos() {
     return myYPos;
   }
+
 }
